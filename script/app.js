bc9d61991c00486daabef710ebf777e5202df567
Santiago Mesa <thiagomesa21@gmail.com>
2026-03-12 19:02:38 -0500

    Agregar variables para elementos del DOM en app.js

---
+++ script/app.js
@@ -0,0 +1,11 @@
+const titulo = document.getElementById("titulo");
+const mensajePrincipal = document.getElementById("mensajePrincipal");
+const btnCambiarTexto = document.getElementById("btnCambiarTexto");
+const btnCambiarClase = document.getElementById("btnCambiarClase");
+const btnCrearElemento = document.getElementById("btnCrearElemento");
+const btnEliminarUltimo = document.getElementById("btnEliminarUltimo");
+const listaTareas = document.getElementById("listaTareas");
+const formularioRegistro = document.getElementById("formularioRegistro")
+const nombreInput = document.getElementById("nombre")
+const salidaFormulario = document.getElementById("salidaFormulario")
+
